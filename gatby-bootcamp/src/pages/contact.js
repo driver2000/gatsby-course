a6a83f0bd0a8b0578c60b1f575f653e7ed59a8f9
Daniele Iervolino <daniele.iervolino@statpro.com>
2019-11-02 23:13:30 +0100

    [GB-2] added some new pages (#1)
    * add pages
    
    * Add more pages

---
+++ gatby-bootcamp/src/pages/contact.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ContactPage = () => {
+  return (
+    <div>
+      <h1> Contact </h1>
+      <p>The best way you reach me is via @driver2000 on Twitter</p>
+    </div>
+  );
+};
+
+export default ContactPage;
+ 
---
+++ gatby-bootcamp/src/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const indexPage = () => {
+  return (
+    <div>
+      <h1>Hello.</h1>
+      <h2>I'm Daniele, a backend developer living in beautiful Naples.</h2>
+    </div>
+  );
+};
+
+export default indexPage;
