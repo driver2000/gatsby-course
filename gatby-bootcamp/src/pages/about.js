51d8e776bfcfda8da0af66f17af624703dce5ecf
Daniele Iervolino <raiervol@gmail.com>
2019-11-02 22:11:38 +0000

    Add more pages

---
+++ gatby-bootcamp/src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1> About Me </h1>
+      <p>Hi! I'm a dev and I'm trying GatsbyJS for the first time!</p>
+    </div>
+  );
+};
+
+export default AboutPage;
