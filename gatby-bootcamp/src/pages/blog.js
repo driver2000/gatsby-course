81943078a5dd726b06a4e96002a55d3f66fa5663
Daniele Iervolino <raiervol@gmail.com>
2019-11-02 22:00:40 +0000

    add pages

---
+++ gatby-bootcamp/src/pages/blog.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const BlogPage = () => {
+  return (
+    <div>
+      <h1> Blog </h1>
+      <p>Posts will show right here later on.</p>
+    </div>
+  );
+};
+
+export default BlogPage;
