4908c701a78aa48d83db4205d79cbbb9b60be95b
Daniele Iervolino <raiervol@gmail.com>
2019-11-04 22:00:23 +0000

    add some style

--- gatsby-bootcamp/src/components/header.js
+++ gatsby-bootcamp/src/components/header.js
@@ -1,25 +1,53 @@
 import React from "react";
 import { Link } from "gatsby";
+import headerStyles from "../styles/header.module.scss";
 
 const Header = () => {
   return (
-    <header>
+    <header className={headerStyles.header}>
       <div>
         <h1 style={{ margin: 0 }}>
-          <Link to="/" className="text-white">
+          <Link to="/" className={headerStyles.title}>
             GatsbyJS Bootcamp
           </Link>
         </h1>
         <nav>
-          <ul>
+          <ul className={headerStyles.navList}>
             <li>
-              <Link to="/blog">Blog</Link>
+              <Link
+                className={headerStyles.navItem}
+                activeClassName={headerStyles.activeNavItem}
+                to="/"
+              >
+                Home
+              </Link>
             </li>
             <li>
-              <Link to="/about">About</Link>
+              <Link
+                className={headerStyles.navItem}
+                activeClassName={headerStyles.activeNavItem}
+                to="/blog"
+              >
+                Blog
+              </Link>
             </li>
             <li>
-              <Link to="/contact">Contact</Link>
+              <Link
+                className={headerStyles.navItem}
+                activeClassName={headerStyles.activeNavItem}
+                to="/about"
+              >
+                About
+              </Link>
+            </li>
+            <li>
+              <Link
+                className={headerStyles.navItem}
+                activeClassName={headerStyles.activeNavItem}
+                to="/contact"
+              >
+                Contact
+              </Link>
             </li>
           </ul>
         </nav>
