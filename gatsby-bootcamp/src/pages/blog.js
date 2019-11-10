008eaab98d8fd87c77682d02c0b1e109dd775514
Daniele Iervolino <raiervol@gmail.com>
2019-11-10 13:05:23 +0000

    use graphql to fetch post metadata

--- gatsby-bootcamp/src/pages/blog.js
+++ gatsby-bootcamp/src/pages/blog.js
@@ -1,11 +1,33 @@
 import React from "react";
 import Layout from "../components/layout";
+import { graphql, useStaticQuery } from "gatsby";
 
 const BlogPage = () => {
+  const data = useStaticQuery(graphql`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            id
+            frontmatter {
+              title
+              date
+            }
+          }
+        }
+      }
+    }
+  `);
+  const listItems = data.allMarkdownRemark.edges.map(d => (
+    <li key={d.node.id}>
+      <h2>{d.node.frontmatter.title}</h2>
+      <p>{d.node.frontmatter.date}</p>
+    </li>
+  ));
   return (
     <Layout>
       <h1> Blog </h1>
-      <p>Posts will show right here later on.</p>
+      <ol>{listItems}</ol>
     </Layout>
   );
 };
